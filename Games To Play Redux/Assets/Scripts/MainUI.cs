87e013bf405a8669c0f941f81186d0dfd77a993d
Jnovins112 <jtn9575@rit.edu>
2021-02-14 14:32:31 -0500

    -Initial v1.0 commit

---
+++ Games To Play Redux/Assets/Scripts/MainUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    public SplashLoader loader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hide all of the components
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!loader.loaded) return;
+
+        
+    }
+}
---
+++ Games To Play Redux/Assets/Scripts/SplashLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Diagnostics;
+using System;
+
+public class SplashLoader : MonoBehaviour
+{
+    public GameObject loadingImage;
+    public Slider slider;
+
+    public Stopwatch stopwatch;
+
+    public bool loaded;
+    public int loadTime = 10000;
+    public float progress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loaded = false;
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stopwatch.Stop();
+        TimeSpan ts = stopwatch.Elapsed;
+        stopwatch.Start();
+        double millisPassed = ts.TotalMilliseconds;
+        progress = (float)(millisPassed/loadTime);
+
+        slider.value = progress;
+
+        if(progress >= 1){
+            loaded = true;
+
+            Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+            foreach(Transform t in children){
+                t.gameObject.SetActive(false);
+            }
+            this.enabled= false;
+        }
+    }
+}
